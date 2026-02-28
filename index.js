5a6e642a65718187e8838585fc6a8e4c050fb48a
Sumukha S Bharadwaj <sumukhasbharadwaj@gmail.com>
2026-02-28 23:14:44 +0530

    These are the starting files for the drum kit.

---
+++ index.js
@@ -0,0 +1,56 @@
+
+document.querySelectorAll(".drum").forEach((drumkey) => {
+  drumkey.addEventListener("click", function () {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+});
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+      case "w":
+        let crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+      case "a":
+        let kick = new Audio("./sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      case "s":
+        let snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+      case "d":
+        let tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "j":
+        let tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "k":
+        let tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "l":
+        let tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      default:
+        console.log(buttonInnerHTML);
+    }
+}
+ function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey);
+    let oldColor = activeButton.style.color;
+    activeButton.style.color = "black";
+    setTimeout(function () {
+      activeButton.style.color = oldColor;
+    }, 100);
+  }
